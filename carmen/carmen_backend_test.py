06dde38304cc4aa6044d5874c21cd8e4cd1f1bb6
Rahul Iyer <rahulair@gmail.com>
2024-04-22 15:20:27 -0700

    Add tests for game state mutation tools

--- carmen/carmen_backend_test.py
+++ carmen/carmen_backend_test.py
@@ -6,20 +6,20 @@ from carmen_backend import *
 
 class TestGameStateFunctions(unittest.TestCase):
     GAME_STATES = {
-        '8d354a22-4ee1-4eb5-a594-aa6a60a2d6c5': {
-            'case_id': '8d354a22-4ee1-4eb5-a594-aa6a60a2d6c5',
-            'suspect_name': 'Picasso Peculiar',
-            'current_city': 'Chennai',
-            'stolen_item': "The Louvre's Laughing Mona Lisa",
-            'hops': [
-                'Paris', 
-                'Rome', 
-                'Cairo', 
-                'Tokyo', 
-                'New York', 
-                'Sydney'
+        "8d354a22-4ee1-4eb5-a594-aa6a60a2d6c5": {
+            "case_id": "8d354a22-4ee1-4eb5-a594-aa6a60a2d6c5",
+            "suspect_name": "Picasso Peculiar",
+            "current_city": "Chennai",
+            "stolen_item": "The Louvre's Laughing Mona Lisa",
+            "hops": [
+                "Paris",
+                "Rome",
+                "Cairo",
+                "Tokyo",
+                "New York",
+                "Sydney"
             ],
-            'next_hop': 3
+            "next_hop": 3
         }
     }
 
@@ -38,14 +38,17 @@ class TestGameStateFunctions(unittest.TestCase):
     def tearDownClass(cls):
         cls.remove_game_state_file()
 
+
     def setUp(self):
         with open(GAME_STATE_FILE, "w") as f:
             json.dump(self.GAME_STATES, f)
 
-        self.case_id, _ = next(iter(get_game_states().items()))
+        self.game_states = self.GAME_STATES
+        self.case_id, _ = next(iter(self.game_states.items()))
 
     def tearDown(self):
         self.remove_game_state_file()
+
 
     def test_missing_file_returns_empty_dict(self):
         os.remove(GAME_STATE_FILE)
@@ -59,17 +62,50 @@ class TestGameStateFunctions(unittest.TestCase):
         self.assertEqual(ret, self.GAME_STATES)
 
     def test_state_is_stored_correctly(self):
-        new_game_states = self.GAME_STATES
-        new_game_states[self.case_id]["next_hop"] = 77
 
-        store_game_state(new_game_states[self.case_id])
+        self.game_states[self.case_id]["next_hop"] = 77
 
-        self.assertEquals(get_game_states(), new_game_states)
+        store_game_state(self.game_states[self.case_id])
+
+        self.assertEqual(get_game_states(), self.game_states)
 
     def test_get_game_state(self):
-        game_state = self.GAME_STATES[self.case_id]
+        game_state = self.game_states[self.case_id]
 
-        self.assertEquals(get_game_state(self.case_id), game_state)
-        
+        self.assertEqual(get_game_state(self.case_id), game_state)
+
+    def test_fetch_game_state_tool(self):
+        self.assertEqual(
+            fetch_game_state.invoke(
+                {
+                    "case_id": self.case_id
+                }
+            ),
+            json.dumps(self.game_states[self.case_id])
+        )
+
+    def test_set_current_city_tool(self):
+        set_current_city.invoke(
+            {
+                "case_id": self.case_id,
+                "current_city": "Bangalore"
+            },
+        )
+
+        new_game_state = get_game_state(self.case_id)
+        self.assertEqual(new_game_state["current_city"], "Bangalore")
+
+    def test_update_game_state_tool(self):
+        update_game_state.invoke(
+            {
+                "case_id": self.case_id,
+                "key": "next_hop",
+                "value": 77
+            },
+        )
+
+        new_game_state = get_game_state(self.case_id)
+        self.assertEqual(new_game_state["next_hop"], 77)
+
 if __name__ == "__main__":
     unittest.main()
